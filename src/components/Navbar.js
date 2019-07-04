98ebcc9e966c088fe014e53e9b6b60ee93caad92
phubao1999 <phutuongbao1999@gmail.com>
2019-07-05 04:10:39 +0700

    This is Shop Of Phone

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <Link to="/">
+            <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+            <li className="nav-item ml-5">
+                <Link to="/" className="nav-link">
+                    product
+                </Link>
+            </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+            <ButtonContainer>
+                <span>
+                    <i className="fas fa-cart-plus mr-2" />
+                </span>
+                my cart
+            </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    );
+  }
+}
+
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link{
+        color:var(--mainWhite)!important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`
+
+export default Navbar;
