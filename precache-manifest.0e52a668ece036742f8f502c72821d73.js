94bc03625f25fc77f2f962eb7132c77957125a39
phubao1999 <phutuongbao1999@gmail.com>
2019-07-05 04:17:49 +0700

    Updates

---
+++ precache-manifest.0e52a668ece036742f8f502c72821d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06e528e433f10b33e48ac71165a886f",
+    "url": "/phone-shope/index.html"
+  },
+  {
+    "revision": "4f00115c19dc6320450f",
+    "url": "/phone-shope/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "a51b455936bbcdbf795f",
+    "url": "/phone-shope/static/css/main.3676ac76.chunk.css"
+  },
+  {
+    "revision": "4f00115c19dc6320450f",
+    "url": "/phone-shope/static/js/2.5001512d.chunk.js"
+  },
+  {
+    "revision": "a51b455936bbcdbf795f",
+    "url": "/phone-shope/static/js/main.4afc1a67.chunk.js"
+  },
+  {
+    "revision": "eca1d76d69125b9a2e53",
+    "url": "/phone-shope/static/js/runtime~main.a9aeeb49.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/phone-shope/static/media/logo.9f7e506d.svg"
+  }
+]);
